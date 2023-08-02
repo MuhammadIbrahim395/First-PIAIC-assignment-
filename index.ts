e004df71f1344cd58fec7d69f5e8adf552b8d2f0
MuhammadIbrahim395 <140939092+MuhammadIbrahim395@users.noreply.github.com>
2023-08-02 23:25:46 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,30 @@
+console.log("This Is My First Assignment I Perform Today");
+
+var message= "Hey Abu Huraiirah,I Have Started Learning TYPESCRIPT"
+console.log(message);
+var Name:string ="My Name Is Muhammad Ibrahim";
+console.log(Name);
+console.log("sum of numbers");
+let x1= 1;
+let x2= 2;
+let x3= 3;
+let x4= 4;
+let x5= 5;
+let x6= 6;
+let x7= 7;
+let x8= 8;
+let x9= 9;
+let x10= 10;
+let y1=(x1+x2+x3+x4+x5+x6+x7+x8+x9+x10);
+console.log(y1);
+console.log("After Apply Multiply");
+let y2=(x1*x2*x3*x4*x5*x6*x7*x8*x9*x10);
+console.log(y2);
+console.log("After Apply Sutraction");
+let y3=(x1-x2-x3-x4-x5-x6-x7-x8-x9-x10);
+console.log(y3);
+console.log("After Apply Division");
+let x11=8;
+let x12=2;
+let result = x11 / x12;
+console.log(result);
